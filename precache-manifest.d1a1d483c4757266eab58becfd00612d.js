05e4da52357906bf175917306eb8a9a3cae3f0eb
Jenniline <jennilineebai@gmail.com>
2020-07-17 16:38:50 +0100

    Updates

---
+++ precache-manifest.d1a1d483c4757266eab58becfd00612d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563dd2903a3ac1ccb418444bb00c0a12",
+    "url": "/DisplayUserInfoReact/index.html"
+  },
+  {
+    "revision": "a52aa2f8e4d85acb139e",
+    "url": "/DisplayUserInfoReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75b90fb170df266f86d4",
+    "url": "/DisplayUserInfoReact/static/js/2.d883e009.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/DisplayUserInfoReact/static/js/2.d883e009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52aa2f8e4d85acb139e",
+    "url": "/DisplayUserInfoReact/static/js/main.5454815a.chunk.js"
+  },
+  {
+    "revision": "1d89972fcefaea271bb6",
+    "url": "/DisplayUserInfoReact/static/js/runtime-main.2627023c.js"
+  }
+]);
